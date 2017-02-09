f5e0f2795f395540a2c6a0d50656bb3aec674d06
Przemyslaw Kobylanski <przemko@mac.com>
2017-02-09 08:44:31 +0100

    poprawki

--- spark/bubbles/bubbles.adb
+++ spark/bubbles/bubbles.adb
@@ -2,9 +2,12 @@ package body Bubbles with SPARK_Mode is
 
    procedure Sort (T : in out Table) is
    begin
-      for I in reverse T'First .. T'Last - 1 loop
-         pragma Loop_Invariant (Is_Sorted (T (I + 1 .. T'Last)));
-         for J in T'First .. I loop
+      for I in reverse T'First .. T'Last loop
+
+         pragma Loop_Invariant (Is_Sorted (T (I  .. T'Last)));
+
+         for J in T'First .. I - 1 loop
+
             if T (J) > T (J + 1) then
                declare
                   Temp : Integer := T (J);
@@ -13,8 +16,11 @@ package body Bubbles with SPARK_Mode is
                   T (J + 1) := Temp;
                end;
             end if;
-            pragma Loop_Invariant (Is_Maximum (T (T'First .. I), T (I)));
+
+            pragma Loop_Invariant (Is_Maximum (T (T'First .. J + 1), T (J + 1)));
+
          end loop;
+
       end loop;
    end Sort;
 
