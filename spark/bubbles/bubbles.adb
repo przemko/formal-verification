ad21c859b32558158a16be2964975367a1d66724
Przemyslaw Kobylanski <przemko@mac.com>
2017-02-17 13:45:36 +0100

    poprawki

--- spark/bubbles/bubbles.adb
+++ spark/bubbles/bubbles.adb
@@ -1,13 +1,13 @@
 package body Bubbles with SPARK_Mode is
 
-   procedure Swap (T : in out Table; I : in Integer; J : in Integer) is
+   procedure Swap (T : in out Table; I : in Positive; J : in Positive) is
       Temp : Integer := T (I);
    begin
       T (I) := T (J);
       T (J) := Temp;
    end Swap;
 
-   procedure Bubble (T : in out Table; I : in Integer) is
+   procedure Bubble (T : in out Table; I : in Positive) is
       Prev_T : Table(T'Range) := T with Ghost;
    begin
       for J in T'First .. I - 1 loop
@@ -28,13 +28,14 @@ package body Bubbles with SPARK_Mode is
    procedure Sort (T : in out Table) is
    begin
 
-      for I in reverse T'Range loop
+      for I in reverse T'Range  loop
 
          Bubble (T, I);
 
-         pragma Loop_Invariant (for all J in T'First .. I =>  T(J) <= T (I));
-         pragma Assert (if I < T'Last then T(I) <= T(I + 1)); -- dla ponizszego INV
-         pragma Loop_Invariant (for all J in I .. T'Last - 1 => T(J) <= T (J + 1));
+         pragma Loop_Invariant
+           (for all J in I .. T'Last =>
+              (for all K in T'First .. J =>
+                   T(K) <= T(J)));
 
 
       end loop;
--- spark/bubbles/bubbles.ads
+++ spark/bubbles/bubbles.ads
@@ -1,21 +1,21 @@
 package Bubbles with SPARK_Mode is
 
-   type Table is array (Integer range <>) of Integer;
+   type Table is array (Positive range <>) of Integer;
 
-   procedure Swap (T : in out Table; I : in Integer; J : in Integer)
+   procedure Swap (T : in out Table; I : in Positive; J : in Positive)
      with
        Depends => (T => (T, I, J)),
        Pre => T'Length > 1 and then I in T'Range and then J in T'Range and then I /= J,
      Post => (for all K in T'Range => (if K /= I and then K /= J then T (K) = T'Old (K))) and then
                 T (I) = T'Old (J) and then T (J) = T'Old (I);
 
-   procedure Bubble (T : in out Table; I : in Integer)
+   procedure Bubble (T : in out Table; I : in Positive)
      with
        Depends => (T => (T, I)),
        Pre => T'Length > 0
        and then I in T'Range
-       and then T'First > Integer'First
-       and then T'Last < Integer'Last,
+       and then T'First > Positive'First
+       and then T'Last < Positive'Last,
        Post =>
          (for all J in T'First .. I => T(J) <= T(I))
          and then (for all J in I + 1 .. T'Last => T (J) = T'Old (J));
@@ -25,10 +25,10 @@ package Bubbles with SPARK_Mode is
        Depends => (T => T),
      Pre =>
        T'Length >= 1 and then
-       T'Last > Integer'First and then
-       T'Last < Integer'Last - 1  and then
-       T'First > Integer'First and then
-       T'First < Integer'Last - 1,
+       T'Last > Positive'First and then
+       T'Last < Positive'Last  and then
+       T'First > Positive'First and then
+       T'First < Positive'Last,
        Post => (for all I in T'First .. T'Last - 1 => T(I) <= T(I + 1));
 
 end Bubbles;
