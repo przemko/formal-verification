ed777ba135f47e3676555a2e9e1344c1c423ac8a
Przemyslaw Kobylanski <przemko@mac.com>
2017-02-21 16:08:16 +0100

    dodano dowod bycia permutacja

--- spark/bubbles/bubbles.adb
+++ spark/bubbles/bubbles.adb
@@ -1,21 +1,48 @@
 package body Bubbles with SPARK_Mode is
 
+   -- ponizsza funkcja Perm_Transitive zostala zaczerpnieta z
+   -- J.W. McCormick, P.C. Chain. Building High Integrity Applications
+   -- with SPARK.
+
+   function Perm_Transitive (A, B, C : Table) return Boolean
+     with
+       Global => null,
+       Post =>
+         (if Perm_Transitive'Result
+          and then Perm (A, B)
+          and then Perm(B, C)
+                  then Perm(A, C)),
+         Ghost => True,
+         Import => True;
+
    procedure Swap (T : in out Table; I : in Positive; J : in Positive) is
       Temp : Integer := T (I);
+      Old_T : constant Table := T with Ghost;
    begin
       T (I) := T (J);
       T (J) := Temp;
+      pragma Assume (Perm(Old_T, T));
    end Swap;
 
    procedure Bubble (T : in out Table; I : in Positive) is
+      Prev_T : Table (T'Range) with Ghost;
    begin
+
+      pragma Assume (Perm (T, T));
+
       for J in T'First + 1 .. I loop
+
+         Prev_T := T;
 
          if T (J - 1) > T (J) then
 
             Swap (T, J - 1, J);
 
          end if;
+
+         pragma Assume (Perm_Transitive (T'Loop_Entry, Prev_T, T));
+
+         pragma Loop_Invariant (Perm (T'Loop_Entry, T));
 
          pragma Loop_Invariant
            (for some K in T'First .. J =>
@@ -34,11 +61,20 @@ package body Bubbles with SPARK_Mode is
    end Bubble;
 
    procedure Sort (T : in out Table) is
+      Prev_T : Table (T'Range) with Ghost;
    begin
+
+      pragma Assume (Perm (T, T));
 
       for I in reverse T'Range loop
 
+         Prev_T := T;
+
          Bubble (T, I);
+
+         pragma Assume (Perm_Transitive (T'Loop_Entry, Prev_T, T));
+
+         pragma Loop_Invariant (Perm (T'Loop_Entry, T));
 
          pragma Loop_Invariant
            (for all J in I .. T'Last =>
--- spark/bubbles/bubbles.ads
+++ spark/bubbles/bubbles.ads
@@ -2,12 +2,29 @@ package Bubbles with SPARK_Mode is
 
    type Table is array (Positive range <>) of Integer;
 
+   -- ponizsza funkcja Perm zostala zaczerpnieta z
+   -- J.W. McCormick, P.C. Chain. Building High Integrity Applications
+   -- with SPARK.
+
+   function Perm (A : in Table; B : in Table) return Boolean
+     with
+       Global => null,
+       Ghost => True,
+       Import => True;
+
    procedure Swap (T : in out Table; I : in Positive; J : in Positive)
      with
-       Depends => (T => (T, I, J)),
-       Pre => T'Length > 1 and then I in T'Range and then J in T'Range and then I /= J,
-     Post => (for all K in T'Range => (if K /= I and then K /= J then T (K) = T'Old (K))) and then
-                T (I) = T'Old (J) and then T (J) = T'Old (I);
+       Depends =>
+         (T => (T, I, J)),
+         Pre =>
+           T'Length > 1 and then
+           I in T'Range and then
+           J in T'Range and then I /= J,
+         Post =>
+           Perm (T'Old, T) and then
+           (for all K in T'Range =>
+              (if K /= I and then K /= J then T (K) = T'Old (K))) and then
+     T (I) = T'Old (J) and then T (J) = T'Old (I);
 
    procedure Bubble (T : in out Table; I : in Positive)
      with
@@ -17,6 +34,7 @@ package Bubbles with SPARK_Mode is
        and then T'First > Positive'First
        and then T'Last < Positive'Last,
        Post =>
+         Perm (T'Old, T) and then
          (for all J in T'First .. I => T(J) <= T(I))
          and then (for some J in T'First .. I => T(I) = T'Old (J))
          and then (for all J in I + 1 .. T'Last => T (J) = T'Old (J));
@@ -30,8 +48,11 @@ package Bubbles with SPARK_Mode is
        T'Last < Positive'Last  and then
        T'First > Positive'First and then
        T'First < Positive'Last,
-       Post => (for all I1 in T'Range =>
-                  (for all I2 in T'Range =>
-                     (if I1 < I2 then T(I1) <= T(I2))));
+       Post =>
+         Perm (T'Old, T) and then
+     (for all I1 in T'Range =>
+        (for all I2 in T'Range =>
+             (if I1 < I2 then T(I1) <= T(I2))));
 
 end Bubbles;
+
