720865ac4abd8b6960973b613cc068c8a9479890
Przemysław Kobylański <przemko@mac.com>
2017-02-22 09:26:51 +0100

    poprawki

--- spark/queens/main.adb
+++ spark/queens/main.adb
@@ -6,10 +6,9 @@ procedure Main is
    N : Integer;
 begin
    loop
-      Put_Line ("Podaj rozmiar planszy (zero aby zakonczyc):");
+      Put_Line ("Podaj rozmiar planszy (zero aby zakonczyc): ");
       Get (N);
       exit when N = 0;
-      Put_Line ("Liczba ustawien hetmanow = " &
-                  Natural'Image (Queens (N)));
+      Put_Line ("Liczba ustawien hetmanow = " & Natural'Image (Queens (N)));
    end loop;
 end Main;
