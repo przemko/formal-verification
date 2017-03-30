703d87f74631dc93385ba447a0363bbdfef4f05e
Przemyslaw Kobylanski <przemko@mac.com>
2017-03-30 12:47:21 +0200

    uproszono

--- spark/bubbles/main.adb
+++ spark/bubbles/main.adb
@@ -2,7 +2,7 @@ with Ada.Text_IO; use Ada.Text_IO;
 with Bubbles; use Bubbles;
 
 procedure Main is
-   T : Table (1 .. 5) := (5, 2, 3, 4, 1);
+   T : Table := (5, 2, 3, 4, 1);
 begin
    Sort (T);
    for I in T'Range loop
