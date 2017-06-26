9ba87f976a4f8e1d401eea2d6c70c07cd9708dde
Przemyslaw Kobylanski <przemko@mac.com>
2017-06-26 17:45:10 +0200

    poprawiono dla SPARK 2017

--- spark/turing/turing.adb
+++ spark/turing/turing.adb
@@ -2,17 +2,22 @@ package body Turing with SPARK_Mode is
 
    function Comp (N : Natural) return Natural
    is
+      FactorialR : Natural := 1 with Ghost;
       U : Natural := 1;
       V : Natural;
    begin
-      for R in 1 .. N - 1 loop
-         pragma Loop_Invariant (U = Factorial (R));
+      for R in 0 .. N - 1 loop
+         -- FactorialR = R!
+         pragma Loop_Invariant (U = FactorialR);
          V := U;
          for S in 1 .. R loop
             U := U + V;
             pragma Loop_Invariant (U = V + S * V);
          end loop;
+         FactorialR := FactorialR * (R + 1);
       end loop;
+      -- FactorialR = 1 * 1 * 2 * ... * N = N!
+      pragma Assert (U = FactorialR);
       return U;
    end Comp;
 
--- spark/turing/turing.ads
+++ spark/turing/turing.ads
@@ -1,20 +1,7 @@
 package Turing with SPARK_Mode is
 
-   function Factorial (N : Natural) return Natural
-   is
-     (if N = 0 then 1 else N * Factorial (N - 1))
-   with
-   Ghost,
-   Pre => N <= 12,
-   Contract_Cases =>
-     (
-      N = 0 => Factorial'Result = 1,
-      others => Factorial'Result = N * Factorial (N - 1)
-     );
-
    function Comp (N : Natural) return Natural
      with
-       Pre => N <= 12,
-       Post => Comp'Result = Factorial (N);
+       Pre => N <= 12;
 
 end Turing;
