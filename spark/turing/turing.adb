62082c169c4d6066787b7117af55b0681027a082
Przemysław Kobylański <przemko@mac.com>
2017-11-14 14:47:25 +0100

    poprawki

--- spark/turing/turing.adb
+++ spark/turing/turing.adb
@@ -2,11 +2,13 @@ package body Turing with SPARK_Mode is
 
    function Comp (N : Natural) return Natural
    is
+      Factorial : Natural := 1 with Ghost;
       U : Natural := 1;
       V : Natural;
    begin
       for R in 1 .. N - 1 loop
-         pragma Loop_Invariant (U = Factorial (R));
+         Factorial := Factorial * R;
+         pragma Loop_Invariant (U = Factorial);
          V := U;
          for S in 1 .. R loop
             U := U + V;
