c9b006c4bb4a289c3ffb87cda82f969d8b9da9ba
Przemyslaw Kobylanski <przemko@mac.com>
2018-12-07 12:04:51 +0100

    poprawki

--- spark/f91/f91.adb
+++ spark/f91/f91.adb
@@ -1,7 +1,8 @@
-pragma SPARK_Mode;
-
 function F91 (N : Integer) return Integer
-is
+  with 
+  SPARK_Mode,
+  Post => (if N <= 100 then F91'Result = 91 else F91'Result = N - 10)
+     is
 begin
    if N <= 100 then
       return F91 (F91 (N + 11));
--- spark/f91/f91.ads
+++
@@ -1,5 +0,0 @@
-pragma SPARK_Mode;
-
-function F91 (N : Integer) return Integer
-  with
-    Post => (if N <= 100 then F91'Result = 91 else F91'Result = N - 10);
